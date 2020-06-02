5ff4e456d2c9ca0cb48c23eb666bee9bfe0892bf
Magnus Smed <magnussmed1@gmail.com>
2020-06-02 22:29:37 +0200

    first version works

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 npm-fix:
-	rm -rf /Users/magnussmed/Documents/GitHub/chrome-tinder/assets/node_modules/public-encrypt/test/test_key.pem
-	rm -rf /Users/magnussmed/Documents/GitHub/chrome-tinder/assets/node_modules/public-encrypt/test/test_rsa_pubkey.pem
-	rm -rf /Users/magnussmed/Documents/GitHub/chrome-tinder/assets/node_modules/public-encrypt/test/test_rsa_privkey.pem
+	rm -rf /Users/magnussmed/Documents/GitHub/tinder-for-twitch/assets/node_modules/public-encrypt/test/test_key.pem
+	rm -rf /Users/magnussmed/Documents/GitHub/tinder-for-twitch/assets/node_modules/public-encrypt/test/test_rsa_pubkey.pem
+	rm -rf /Users/magnussmed/Documents/GitHub/tinder-for-twitch/assets/node_modules/public-encrypt/test/test_rsa_privkey.pem
--- assets/js/content.js
+++ assets/js/content.js
@@ -1,43 +1,53 @@
-setInterval(function(){
-	var el = document.getElementsByClassName("CenterAlign Pos(a) B(0) Maw(100%)");
-	if ( el !== null ) {
-		for ( a of el ) {
-			a.firstElementChild.innerHTML = "Din mor";
-		}
-	}
+$( document ).ready(function() {
+	'use strict';
 
-	var el = document.getElementsByClassName("Maw(340px)");
-	if ( el !== null ) {
-		for ( a of el ) {
-			a.firstElementChild.innerHTML = "Din mor";
-		}
-	}
+	var champions = ["Aatrox","Ahri","Akali","Alistar","Amumu","Anivia","Annie","Aphelios","Ashe","Aurelion Sol","Azir","Bard","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","Cho'Gath","Corki","Darius","Diana","Dr. Mundo","Draven","Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","Jarvan IV","Jax","Jayce","Jhin","Jinx","Kai'Sa","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","Kha'Zix","Kindred","Kled","Kog'Maw","LeBlanc","Lee Sin","Leona","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","Master Yi","Miss Fortune","Mordekaiser","Morgana","Nami","Nasus","Nautilus","Neeko","Nidalee","Nocturne","Nunu and Willump","Olaf","Orianna","Ornn","Pantheon","Poppy","Pyke","Qiyana","Quinn","Rakan","Rammus","Rek'Sai","Renekton","Rengar","Riven","Rumble","Ryze","Sejuani","Senna","Sett","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Sylas","Syndra","Tahm Kench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","Twisted Fate","Twitch","Udyr","Urgot","Varus","Vayne","Veigar","Vel'Koz","Vi","Viktor","Vladimir","Volibear","Warwick","Wukong","Xayah","Xerath","Xin Zhao","Yasuo","Yorick","Yuumi","Zac","Zed","Ziggs","Zilean","Zoe","Zyra"];
 
-	var el = document.getElementsByClassName("Fz($xl) Fw($bold) Fxs(1) Flw(w) Pend(8px) M(0) D(i)");
-	if ( el !== null ) {
-		for ( a of el ) {
-			a.innerHTML = "Din mor";
-		}
-	}
+	var cl = champions.length;
 
-	var el = document.getElementsByClassName("chatNavBar__connectionInfo");
-	if ( el !== null ) {
-		for ( a of el ) {
-			a.innerHTML = "Din mor";
-		}
-	}
+	setInterval( function() {
+		$( 'body .matchListItem .Ell' ).each(function(e) {
+			var champ = champions[Math.floor(Math.random() * cl)];
+			var current = $( this ).text();
+			if ( jQuery.inArray( current, champions ) == -1 ) {
+				$( this ).html( champ );
+			}
+		});
 
-	// var x = document.getElementsByClassName("Fz($xl) Fw($bold) Fxs(1) Flw(w) Pend(8px) M(0) D(i)");
-	// if ( x !== null ) {
-	// 	for ( a of x ) {
-	// 		a.innerHTML = "Din mor";
-	// 	}
-	// }
-	//
-	// var x = document.getElementsByClassName("h3.Maw(340px)");
-	// if ( x !== null ) {
-	// 	for ( a of x ) {
-	// 		a.innerHTML = "Din mor";
-	// 	}
-	// }
-}, 100);
+		$( 'body .profileCard__card h1' ).each(function(e) {
+			var champ = champions[Math.floor(Math.random() * cl)];
+			var current = $( this ).text();
+			if ( jQuery.inArray( current, champions ) == -1 ) {
+				$( this ).html( champ );
+			}
+		});
+
+		$( 'body .chat h3 span' ).each(function(e) {
+			var champ = champions[Math.floor(Math.random() * cl)];
+			var current = $( this ).text();
+			if ( jQuery.inArray( current, champions ) == -1 ) {
+				$( this ).html( champ );
+			}
+		});
+
+		$( 'body .messageListItem .messageListItem__name' ).each(function(e) {
+			var champ = champions[Math.floor(Math.random() * cl)];
+			var current = $( this ).text();
+			if ( jQuery.inArray( current, champions ) == -1 ) {
+				$( this ).html( champ );
+			}
+		});
+
+		$( 'body .profileCard__card .BreakWord' ).each(function(e) {
+			var champ = champions[Math.floor(Math.random() * cl)];
+			var current = $( this ).text();
+			if ( jQuery.inArray( current, champions ) == -1 ) {
+				$( this ).html( champ );
+			}
+		});
+
+		$( 'body .chatNavBar__connectionInfo' ).each(function(e) {
+			$( this ).remove();
+		});
+	}, 150 );
+});
